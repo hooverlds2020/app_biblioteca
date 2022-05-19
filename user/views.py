a13f4bdba9deed463cf0a8d4c43dc7f6882f72c2
Roberto <hooverlds93@gmail.com>
2022-05-19 06:47:36 -0500

    version2

--- user/views.py
+++ user/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
+from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import AllowAny
+
 # Create your views here.
 from .serializers import UserSerializer
 from .models import User
@@ -10,3 +13,10 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset =  User.objects.all()
     permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == "create":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsOwnerOrReadOnly]
+        return [permission() for permission in permission_classes]
